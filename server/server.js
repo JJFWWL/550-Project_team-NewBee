b79528a7e4ab224f1bd3d390cc5f373a8754ae36
JJFWWL <hawkj.fan@gmail.com>
2022-03-27 14:55:45 -0400

    JF add route functions 20220327

--- server/server.js
+++ server/server.js
@@ -10,12 +10,21 @@ const app = express();
 
 // whitelist localhost 3000
 app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
-
-// Route 3 - register as GET 
-app.get('/friend_business/:name', routes.friend_business)
-// Route 3.1 - register as GET 
+//2.1 user login
+app.get('/login', routes.login)
+// 2.2 
+app.get('/friend_business/:userid', routes.friend_business)
+// 2.3
 app.get('/friend_connection/:id', routes.friend_connection)
 
+// 3.1
+app.get('/star_sci/:choice', routes.star_sci)
+// 3.2
+app.get('/price_sci/:choice', routes.price_sci)
+// 3.3
+app.get('/avg_sci/:choice', routes.avg_sci)
+// 3.4
+app.get('/cat_map/:choice', routes.cat_map)
 
 app.listen(config.server_port, () => {
     console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
