1a495f05890950b6bd2935a3b7687df86dd418ba
Yusheng Ding <yding3@seas.upenn.edu>
2022-04-09 10:05:04 -0400

    changed fetch path to match with server side

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -7,7 +7,7 @@ class MenuBar extends React.Component {
     return (
       <Menu theme="dark" mode="horizontal">
         <Menu.Item key="1" icon={<BankOutlined />}><a href="/">Home</a></Menu.Item>
-        <Menu.Item key="2" icon={<RedditOutlined />}><a href="/restaurants">Restaurant Recommender</a></Menu.Item>
+        <Menu.Item key="2" icon={<RedditOutlined />}><a href="/businesses">Restaurant Recommender</a></Menu.Item>
         <Menu.Item key="3" icon={<TeamOutlined />}><a href="/friends">Find Friends</a></Menu.Item>
         <Menu.Item key="4" icon={<SketchCircleFilled />}><a href="/scientists">Restaurant Scientists</a></Menu.Item>
 
