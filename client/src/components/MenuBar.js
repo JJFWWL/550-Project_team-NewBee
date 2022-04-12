5b2a098cb09ff5c05091e889fb18290fbabb47c6
Yusheng Ding <yding3@seas.upenn.edu>
2022-04-12 00:04:26 -0400

    added feature to let the menu bar stick on the top

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Menu } from "antd";
+import { Menu, Affix } from "antd";
 import { BankOutlined, RedditOutlined, TeamOutlined, SketchCircleFilled } from '@ant-design/icons';
 
 class MenuBar extends React.Component {
   render() {
     return (
-      <Menu theme="dark" mode="horizontal">
+      <Affix><Menu theme="dark" mode="horizontal">
         <Menu.Item key="1" icon={<BankOutlined />}><a href="/" style={{fontFamily:'cursive'}}>Home</a></Menu.Item>
         <Menu.Item key="2" icon={<RedditOutlined />}><a href="/businesses" style={{fontFamily:'cursive'}}>Restaurant Recommender</a></Menu.Item>
         <Menu.Item key="3" icon={<TeamOutlined />}><a href="/friends" style={{fontFamily:'cursive'}}>Find Friends</a></Menu.Item>
         <Menu.Item key="4" icon={<SketchCircleFilled />}><a href="/scientists" style={{fontFamily:'cursive'}}>Restaurant Scientists</a></Menu.Item>
 
-      </Menu>
+      </Menu></Affix>
     )
   }
 }
